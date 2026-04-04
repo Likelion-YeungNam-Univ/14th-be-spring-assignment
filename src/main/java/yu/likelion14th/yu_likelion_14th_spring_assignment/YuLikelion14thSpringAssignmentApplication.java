77ed459d7ad22ed3cd9d46fceb19a9fb4f05a071
JUNG CHOI <chlwjd0803@naver.com>
2026-04-04 22:48:24 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/yu/likelion14th/yu_likelion_14th_spring_assignment/YuLikelion14thSpringAssignmentApplication.java
@@ -0,0 +1,13 @@
+package yu.likelion14th.yu_likelion_14th_spring_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuLikelion14thSpringAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuLikelion14thSpringAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yu/likelion14th/yu_likelion_14th_spring_assignment/YuLikelion14thSpringAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package yu.likelion14th.yu_likelion_14th_spring_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuLikelion14thSpringAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
